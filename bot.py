9b75f8c1a2a4d081f4ee2965b14f65289e21b898
avci65 <avci65652@gmail.com>
2026-01-17 18:53:23 +0300

    userbot

--- bot.py
+++ bot.py
@@ -6,10 +6,15 @@ print("API_ID ENV RAW:", repr(os.getenv("API_ID")))
 print("API_HASH ENV RAW:", repr(os.getenv("API_HASH")))
 print("SESSION ENV RAW:", repr(os.getenv("SESSION_STRING"))[:60])
 
-API_ID = int(os.getenv("1838931", "0"))
-API_HASH = os.getenv("d8ed731e06dff557170f3b6a85640522", "").strip()
-SESSION_STRING = os.getenv("1BJWap1wBu5_arM-Cj2Ok8C8ltxa9E-RMa4pHeCej9ZX6XtSp33pNeTNmWt7_Qz0mX53a9xm7-JOFyCwZNsEmglEXfa7c2I-xe0Aw0JVnEpazKEIHchHSZ2pdUtbPhoZfW4v_xjPNh9O-6soLVKNXlQ8EPnx--mCCE8i8Dcr2KAvorBZep238lZIAkkWgsc18LjoE4JNAZgyaj-9Mo47wc-kRv4LJ8Y8gyDYarnG0UJ4oualI9eInU4xqUykO3vjnJDtu5eQWMeO-ePGBuULl32N6zBFkl_0qcXqx-4tL2izX1--v_qSlEBuV-YUNRTS4j9PfmzILCvG31LSa2gPMFhpfwn68V48=", "").strip()
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "").strip()
 
+SESSION_STRING = os.getenv("SESSION_STRING", "")
+SESSION_STRING = SESSION_STRING.replace("\n", "").replace("\r", "").strip()
+
+print("API_ID:", API_ID)
+print("HASH_LEN:", len(API_HASH))
+print("SESSION_LEN:", len(SESSION_STRING))
 
 if API_ID == 0 or not API_HASH or not SESSION_STRING:
     raise ValueError("API_ID / API_HASH / SESSION_STRING ortam değişkenleri eksik!")
