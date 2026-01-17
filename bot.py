049e8efab7714746edfe7d02e46e0d74642447b6
avci65 <avci65652@gmail.com>
2026-01-17 03:04:23 +0300

    userbot

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
 import os
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-SESSION_STRING = os.getenv("SESSION_STRING", "")
+API_ID = int(os.getenv("1838931", "0"))
+API_HASH = os.getenv("d8ed731e06dff557170f3b6a85640522", "")
+SESSION_STRING = os.getenv("1BJWap1wBu0Irb95XzQhfEyoQhIGT6PSRLwBF0jGK-DxG-RLKQVxh4juUSWv_n6_GdUycUB9fpN6iOXYl9G8xeoPGr5uS3BRQxEaZh4NknIS-aW3jw0I3JkC3lDb-ReFdr3uzvKpOAJsYLPaVNB866AwgB9TGyyif3KXjDlqNk20b6VsIOhDCs6PXsJudS8qoUXl11Xzv0NkdSKBble9oIVF5a-DM0OftlNIOG-YvM_lxiLh98-PAnj6Ie56EhrmZ4Kgor_B0oZ85Y3aH2axW_32pXbyTTZ0b5hwPnPpYE8c7GBRynMpqPeNBcRRofBqBp-NhualEr3STVDbO8vu0FWy_Su9OUms=", "")
 
 if not API_ID or not API_HASH or not SESSION_STRING:
     raise ValueError("API_ID / API_HASH / SESSION_STRING ortam değişkenleri eksik!")
 
 client = TelegramClient(
-    session=SESSION_STRING,
-    api_id=API_ID,
-    api_hash=API_HASH
+    StringSession(SESSION_STRING),
+    API_ID,
+    API_HASH
 )
 
 @client.on(events.NewMessage(pattern=r"\.ping"))
 async def ping(event):
-    await event.reply("pong ✅ (Railway)")
+    await event.reply("pong ✅ (Railway Userbot)")
 
-@client.on(events.NewMessage(pattern=r"\.id"))
-async def myid(event):
+@client.on(events.NewMessage(pattern=r"\.alive"))
+async def alive(event):
     me = await client.get_me()
-    await event.reply(f"ID: {me.id}")
+    await event.reply(f"✅ Çalışıyorum!\n@{me.username} | ID: {me.id}")
 
 async def main():
     me = await client.get_me()
-    print(f"Userbot aktif ✅  Kullanıcı: @{me.username} | ID: {me.id}")
+    print(f"Userbot başladı ✅ @{me.username} ({me.id})")
 
 client.start()
 client.loop.run_until_complete(main())
