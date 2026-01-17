7a271dd7f36037b0c26fc4a79811030e8c609f5f
avci65 <avci65652@gmail.com>
2026-01-17 03:29:06 +0300

    userbot

--- bot.py
+++ bot.py
@@ -2,33 +2,22 @@ import os
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-raw_api_id = os.getenv("1838931", "").strip()
-API_HASH = os.getenv("d8ed731e06dff557170f3b6a8564052", "").strip()
+API_ID = int(os.getenv("1838931", "0"))
+API_HASH = os.getenv("d8ed731e06dff557170f3b6a85640522", "").strip()
 SESSION_STRING = os.getenv("1BJWap1wBu0Irb95XzQhfEyoQhIGT6PSRLwBF0jGK-DxG-RLKQVxh4juUSWv_n6_GdUycUB9fpN6iOXYl9G8xeoPGr5uS3BRQxEaZh4NknIS-aW3jw0I3JkC3lDb-ReFdr3uzvKpOAJsYLPaVNB866AwgB9TGyyif3KXjDlqNk20b6VsIOhDCs6PXsJudS8qoUXl11Xzv0NkdSKBble9oIVF5a-DM0OftlNIOG-YvM_lxiLh98-PAnj6Ie56EhrmZ4Kgor_B0oZ85Y3aH2axW_32pXbyTTZ0b5hwPnPpYE8c7GBRynMpqPeNBcRRofBqBp-NhualEr3STVDbO8vu0FWy_Su9OUms=", "").strip()
 
-print("RAW API_ID:", repr(raw_api_id))
-print("API_HASH var mı:", bool(API_HASH))
-print("SESSION_STRING var mı:", bool(SESSION_STRING))
-
-try:
-    API_ID = int(raw_api_id)
-except Exception:
-    API_ID = 0
-
-print("PARSED API_ID:", API_ID)
+print("API_ID:", API_ID)
+print("HASH_LEN:", len(API_HASH))
+print("SESSION_LEN:", len(SESSION_STRING))
 
 if API_ID == 0 or not API_HASH or not SESSION_STRING:
-    raise ValueError("API_ID / API_HASH / SESSION_STRING ortam değişkenleri eksik veya hatalı!")
+    raise ValueError("API_ID / API_HASH / SESSION_STRING ortam değişkenleri eksik!")
 
-client = TelegramClient(
-    StringSession(SESSION_STRING),
-    API_ID,
-    API_HASH
-)
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 @client.on(events.NewMessage(pattern=r"\.ping"))
 async def ping(event):
-    await event.reply("pong ✅ (Railway Userbot)")
+    await event.reply("pong ✅")
 
 client.start()
 print("✅ Userbot başladı")
