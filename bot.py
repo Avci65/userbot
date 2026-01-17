09253e16960f4f8c24a4c5376c61a5b7b88d5a65
avci65 <avci65652@gmail.com>
2026-01-17 04:16:19 +0300

    userbot

--- bot.py
+++ bot.py
@@ -2,15 +2,14 @@ import os
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-print("✅ BU BOT.PY DOSYASI ÇALIŞTI")
+print("API_ID ENV RAW:", repr(os.getenv("API_ID")))
+print("API_HASH ENV RAW:", repr(os.getenv("API_HASH")))
+print("SESSION ENV RAW:", repr(os.getenv("SESSION_STRING"))[:60])
 
 API_ID = int(os.getenv("1838931", "0"))
 API_HASH = os.getenv("d8ed731e06dff557170f3b6a85640522", "").strip()
 SESSION_STRING = os.getenv("1BJWap1wBu0Irb95XzQhfEyoQhIGT6PSRLwBF0jGK-DxG-RLKQVxh4juUSWv_n6_GdUycUB9fpN6iOXYl9G8xeoPGr5uS3BRQxEaZh4NknIS-aW3jw0I3JkC3lDb-ReFdr3uzvKpOAJsYLPaVNB866AwgB9TGyyif3KXjDlqNk20b6VsIOhDCs6PXsJudS8qoUXl11Xzv0NkdSKBble9oIVF5a-DM0OftlNIOG-YvM_lxiLh98-PAnj6Ie56EhrmZ4Kgor_B0oZ85Y3aH2axW_32pXbyTTZ0b5hwPnPpYE8c7GBRynMpqPeNBcRRofBqBp-NhualEr3STVDbO8vu0FWy_Su9OUms=", "").strip()
 
-print("API_ID:", API_ID)
-print("HASH_LEN:", len(API_HASH))
-print("SESSION_LEN:", len(SESSION_STRING))
 
 if API_ID == 0 or not API_HASH or not SESSION_STRING:
     raise ValueError("API_ID / API_HASH / SESSION_STRING ortam değişkenleri eksik!")
